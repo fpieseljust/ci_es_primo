a7a7ba9744d54fdeb49a86fc76977bd07b1b3807
ferrancunyatEljust <ferrancunyat@ieseljust.com>
2024-02-07 09:41:03 +0100

    Eliminem errors de linter a funcions.py

--- src/funcions.py
+++ src/funcions.py
@@ -17,10 +17,11 @@ def es_primo(numero):
 
     if numero < 2:
         return False
-    else:
-        for n in range(2, math.floor(math.sqrt(numero) + 1)):
-            if numero % n == 0:
-                return False
+
+    for n in range(2, math.floor(math.sqrt(numero) + 1)):
+        if numero % n == 0:
+            return False
+
     return True
 
 if __name__=='__main__':
