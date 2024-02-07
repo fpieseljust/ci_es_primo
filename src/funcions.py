e1b618a6f474013a9de922091dc349be92f56300
ferrancunyatEljust <ferrancunyat@ieseljust.com>
2024-02-07 09:38:10 +0100

    Canvis en la funció es_primo

--- src/funcions.py
+++ src/funcions.py
@@ -1,8 +1,21 @@
+"""Módulo que contiene la función es_primo"""
 import math
 
 
 def es_primo(numero):
-    if numero < 2:    
+    """
+   Función para determinar si un número es primo:
+
+
+   Entrada:
+   - entero: número entero a determinar si es primo
+
+
+   Salida:
+   - True/False: en caso de ser primo o no
+   """
+
+    if numero < 2:
         return False
     else:
         for n in range(2, math.floor(math.sqrt(numero) + 1)):
@@ -13,3 +26,4 @@ def es_primo(numero):
 if __name__=='__main__':
     for i in range(2,100):
         print(f'{i} -> {es_primo(i)}')
+
